62ac771e57dd53093698246cbd187258503b8dcf
Yorks Gomez <yorksgomez@gmail.com>
2023-02-22 06:46:40 -0500

    Arreglos

---
+++ src/main/java/edu/eci/arsw/blueprints/AppConfig.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.arsw.blueprints;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScans;
+
+/**
+ *
+ * @author edhawk
+ */
+@TestConfiguration
+@ComponentScan(basePackages = "edu.eci.arsw")
+public class AppConfig {
+    
+}
